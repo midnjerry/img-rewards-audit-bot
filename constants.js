2ab218529b461890941acac9739c7979baf398c8
midnjerry <balderasfam@gmail.com>
2025-05-04 00:15:44 -0500

    Initial commit

---
+++ constants.js
@@ -0,0 +1,5 @@
+export const SOLANA_DECIMALS = 1e9;
+export const MAX_SIGNATURES = 10;
+export const REWARDS_WALLET = 'ChGA1Wbh9WN8MDiQ4ggA5PzBspS2Z6QheyaxdVo3XdW6';
+export const SOLANA_RPC = 'https://api.mainnet-beta.solana.com';
+export const IMG_TOKEN_MINT = 'znv3FZt2HFAvzYf5LxzVyryh3mBXWuTRRng25gEZAjh';
